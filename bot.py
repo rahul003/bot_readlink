9e94ff22794415dc06a26775d679f976e01b6caa
Rahul <rahulhuilgol@gmail.com>
2015-09-13 01:44:31 -0500

    reorganize

--- bot.py
+++
@@ -1,6 +0,0 @@
-app_id = 'Bsg6SB9oVm13qA'
-app_secret = 'cU1Sfopir7oVB5rWd1_7fvjdS-E'
-app_uri = 'https://127.0.0.1:65010/authorize_callback'
-user_agent = '/u/YesIAmTheMorpheus Link_text'
-import praw
-r = praw.Reddit('/u/YesIAmTheMorpheus Link_text')
